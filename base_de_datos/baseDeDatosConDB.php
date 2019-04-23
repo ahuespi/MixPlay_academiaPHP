905b4efe8d4b9a9098e360ee416bd01cb9c02f22
ahuespi <amir.huespi@gmail.com>
2019-04-23 18:17:02 -0300

    add insert & update database items from php

--- base_de_datos/baseDeDatosConDB.php
+++ base_de_datos/baseDeDatosConDB.php
@@ -1,21 +1,29 @@
 <?php
+
+
 require_once './vendor/autoload.php';
 class BaseDeDatos
 {
 
     private $client;
     private $db;
+    private $userCollection;
+
 
     public function __construct()
     {
         $this->client = new MongoDB\Client("mongodb://localhost:27017");
+        // echo "Connection to database successfully \n";
         $this->db = $this->client->dbtesting;
+        // echo "Database mydb selected \n";
+        $this->userCollection = $this->db->Users;
+        // echo "Collection created succsessfully \n";
     }
 
     public function show()
     {
-        // return $this->db->getDatabaseName();
-        return $this->db->getCollectionName();
+        return $this->db->getDatabaseName();
+
     }
 
     public function get($id)
@@ -29,32 +37,43 @@ class BaseDeDatos
     
     public function insert($id,$nombre)
     {
-        
-        $read = $this->read();
-        
-        if(!isset($read[$id])){      
-            $read[$id] = $nombre;
+        try {
+            $this->userCollection->insertOne([
+                '_id' => $id,
+                'name' => $nombre
+            ]);
+            return true;
+        } catch (Exception $th) {
+            return false;
         }
         
-        $this->close($read);
-        }
+    }
         
-        public function update($id,$nombre)
-        {
-            $read = $this->read();
-            if(isset($read[$id])){
-                $read[$id] = $nombre;
-            }
-            $this->close($read);
+    public function update ($id,$nombre)
+    {
+        try {
+            $this->userCollection->replaceOne ([
+                '_id' => $id
+            ],[
+                '_id' => $id,
+                'name' => $nombre
+            ]);
+            return true;
+        } catch (\Throwable $th) {
+            print_r ( $th->getMessage() );
+            return false;
         }
-        
-        public function delete($id)
-        {
-            $read = $this->read();
-            if(isset($read[$id])){
-                unset($read[$id]);
-            }
-            $this->close($read);
+    }
+    
+    public function delete($id)
+    {
+        $this->userCollection->deleteOne([
+            'id' => $id
+        ]);
+    }
+    public function deleteAll($id)
+    {
+        $this->userCollection->deleteMany([$id]);
     }
 
 }
@@ -62,13 +81,15 @@ class BaseDeDatos
 $db = new BaseDeDatos();
 echo "Base de datos: " . $db->show() . "\n";
 
-// $db->insert(1, "amir");
-// $db->insert(2, "pepe");
-// $db->insert(3, "123123123");
-// $db->insert(4, "asdasd");
+$db->insert(6, 'pepe');
+
+$db->update(1, "josesin");
+
+
 // $db->update(1, "sd");
 // $db->update(2, "1010");
-// $db->delete(1, "1010");
+// $db->delete(2);
+
 // $db->insert(5, "hola");
 // echo $db->get(4)."\n";
 
